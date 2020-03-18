baa988883c60dceeff66b073a26a3502d930407f
Kateryna8686 <split2005s@gmail.com>
2020-03-17 20:31:11 -0400

    project and class created

---
+++ src/Test/TestClass.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class TestClass {
+	
+	public static void main (String []args) {
+		
+		System.out.println ("Hello");
+		System.out.println ("Hello World");
+		
+		
+	}
+
+}
